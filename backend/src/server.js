0db2e27228b340d9ec221b5974e75c5f3d2f91b0
adityafajarsy <adityafajar.sy90@gmail.com>
2026-07-30 14:43:25 +0700

    feat: initialize backend server with express and root project scripts

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,14 +2,27 @@ import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js'
+import path from "path";
 
 const app = express()
+const _dirname = path.resolve();
 
 dotenv.config();
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes)
 
-app.listen(process.env.PORT, () => {
-    console.log(`server running on port ${process.env.PORT}`)
+//make ready for deployment
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(_dirname, "../frontend/dist")))
+}
+
+app.get("*", (_,res) => {
+    res.sendFile(path.join(_dirname, "../frontend/", "dist", "index.html"))
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`)
 })
