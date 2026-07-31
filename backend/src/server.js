8eedc246fe725169e1d8ac28c0e0faab73600627
adityafajarsy <adityafajar.sy90@gmail.com>
2026-07-31 09:01:04 +0700

    feat: add db
    integrating mongodb, newUser

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,28 +1,34 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import authRoutes from './routes/auth.route.js';
-import messageRoutes from './routes/message.route.js'
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import path from "path";
+import connectDB from "./lib/db.js";
 
-const app = express()
+const app = express();
 const _dirname = path.resolve();
 
 dotenv.config();
 
-app.use('/api/auth', authRoutes);
-app.use('/api/messages', messageRoutes)
+app.use(express.json());
+app.use(cookieParser());
 
-//make ready for deployment
-if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(_dirname, "../frontend/dist")))
-}
-
-app.get("*", (_,res) => {
-    res.sendFile(path.join(_dirname, "../frontend/", "dist", "index.html"))
-})
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 const PORT = process.env.PORT || 3000;
 
+//make ready for deployment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(_dirname, "../frontend/dist")));
+}
+
+app.get("*", (_, res) => {
+  res.sendFile(path.join(_dirname, "../frontend/", "dist", "index.html"));
+});
+
 app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`)
-})
+  console.log(`server running on port ${PORT}`);
+  connectDB();
+});
