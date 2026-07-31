ec0ba0c3037026a46d04ddf8d711e3029101650d
adityafajarsy <adityafajar.sy90@gmail.com>
2026-07-31 16:10:47 +0700

    rate limiting impletemented

---
+++ backend/src/middleware/arcjet.middleware.js
@@ -0,0 +1,35 @@
+import aj from "../lib/arcjet.js";
+import { isSpoofedBot } from "@arcjet/inspect";
+
+export const arcjetProtection = async (req, res, next) => {
+  try {
+    const decision = await aj.protect(req);
+
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        return res.status(429).json({
+          status: "error",
+          message: "Rate limit exceeded. Please try again later.",
+        });
+      } else if (decision.reason.isBot()) {
+        return res.status(403).json({ message: "Bot access denied. " });
+      } else {
+        return res
+          .status(403)
+          .json({ message: "Request blocked due to security policies. " });
+      }
+    }
+
+    if (decision.results.some(isSpoofedBot)) {
+      return res.status(403).json({
+        error: "Spoofed bot detected",
+        message: "Request blocked due to suspicious behavior. ",
+      });
+    }
+
+    next();
+  } catch (error) {
+    console.log("Arcjet Protection Error: ", error);
+    next(error);
+  }
+};
